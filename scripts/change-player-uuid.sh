afee9549877884bff958af5cf4bc5865823aab15
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2023-04-25 21:58:04 +0300

    Script to get all players

--- scripts/change-player-uuid.sh
+++ scripts/change-player-uuid.sh
@@ -3,7 +3,6 @@ OLD_UUID="${1}"
 NEW_UUID="${2}"
 
 SERVER_ROOT_DIR="/srv/papermc"
-SERVER_WORLD_DIR="${SERVER_ROOT_DIR}/world"
 SERVER_PLUGINS_DIR="${SERVER_ROOT_DIR}/plugins"
 SERVER_SCRIPTS_DIR="${SERVER_ROOT_DIR}/scripts"
 
@@ -34,10 +33,10 @@ function move-file() {
     sudo mv "${OLD_FILE}" "${NEW_FILE}"
 }
 
-move-file "${SERVER_WORLD_DIR}/advancements/${OLD_UUID}.json" "${SERVER_ROOT_DIR}/world/advancements/${NEW_UUID}.json"
-move-file "${SERVER_WORLD_DIR}/playerdata/${OLD_UUID}.dat" "${SERVER_ROOT_DIR}/world/playerdata/${NEW_UUID}.dat"
-move-file "${SERVER_WORLD_DIR}/playerdata/${OLD_UUID}.dat_old" "${SERVER_ROOT_DIR}/world/playerdata/${NEW_UUID}.dat_old"
-move-file "${SERVER_WORLD_DIR}/stats/${OLD_UUID}.json" "${SERVER_ROOT_DIR}/world/stats/${NEW_UUID}.json"
+move-file "${WORLD_DIR}/advancements/${OLD_UUID}.json" "${SERVER_ROOT_DIR}/world/advancements/${NEW_UUID}.json"
+move-file "${WORLD_DIR}/playerdata/${OLD_UUID}.dat" "${SERVER_ROOT_DIR}/world/playerdata/${NEW_UUID}.dat"
+move-file "${WORLD_DIR}/playerdata/${OLD_UUID}.dat_old" "${SERVER_ROOT_DIR}/world/playerdata/${NEW_UUID}.dat_old"
+move-file "${WORLD_DIR}/stats/${OLD_UUID}.json" "${SERVER_ROOT_DIR}/world/stats/${NEW_UUID}.json"
 move-file "${SERVER_PLUGINS_DIR}/Essentials/userdata/${OLD_UUID}.yml" "${SERVER_ROOT_DIR}/plugins/Essentials/userdata/${NEW_UUID}.yml"
 
 ${SHELL} "${SERVER_SCRIPTS_DIR}/fix-permissions.sh"
---
+++ scripts/get-players.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+source "/srv/papermc/scripts/common/paths.sh"
+
+function get_userdata_prop() {
+    local FILE="${1}"
+    local PROPERTY="${2}"
+    local PROPERTY_ESC=$(echo "${PROPERTY}" | sed -E 's/([^.]+-[^\.\ ]+)(\.|$)/"\1"\2/g')
+
+    yq -r ".${PROPERTY_ESC}" "${FILE}"
+}
+
+if [ ! -f "/usr/bin/yq" ]; then
+    echo "ERROR: The 'yq' utility must be installed!"
+    exit 1
+fi
+
+for USERDATA_FILE in "${ESSENTIALS_DIR}/userdata/"*; do
+    PLAYER_UUID=$(basename "${USERDATA_FILE}" .yml)
+    PLAYER_NAME=$(get_userdata_prop "${USERDATA_FILE}" "last-account-name")
+    PLAYER_LAST_SEEN_TIMESTAMP=$(get_userdata_prop "${USERDATA_FILE}" "timestamps.logout")
+    PLAYER_LAST_SEEN_DATE=$(date -u -d @"${PLAYER_LAST_SEEN_TIMESTAMP}" +'%Y-%m-%d %H:%M:%S')
+    PLAYER_LAST_IP=$(get_userdata_prop "${USERDATA_FILE}" "ip-address")
+    
+    echo "${PLAYER_NAME}:"
+    echo "   - UUID:      ${PLAYER_UUID}"
+    #echo "   - Last seen: ${PLAYER_LAST_SEEN_DATE}"
+    echo "   - Last IP:   ${PLAYER_LAST_IP}"
+done
