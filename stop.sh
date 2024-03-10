c597bf498ab2bac9e68163ba8494e6754adeefff
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2024-03-10 22:54:18 +0200

    Improved the stop script

--- stop.sh
+++ stop.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-papermc stop
+SERVER_ROOT_DIR=$(pwd)
+source "${SERVER_ROOT_DIR}/scripts/common/paths.sh"
+source "${SERVER_SCRIPTS_COMMON_DIR}/status.sh"
+source "${SERVER_SCRIPTS_COMMON_DIR}/utils.sh"
+
+REASON="${*}"
+
+if ${IS_SERVER_RUNNING}; then
+    send_broadcast_message "The server shutting down..."
+    [ -n "${REASON}" ] && send_broadcast_message "Reason: ${REASON}"
+
+    papermc stop
+fi
