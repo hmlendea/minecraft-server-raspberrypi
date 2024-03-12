4b00acdf1f0d3022b59837a82a5fd86263263780
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2024-03-12 13:47:06 +0200

    Improved the stop script

--- stop.sh
+++ stop.sh
@@ -7,8 +7,14 @@ source "${SERVER_SCRIPTS_COMMON_DIR}/utils.sh"
 REASON="${*}"
 
 if ${IS_SERVER_RUNNING}; then
+    KICK_REASON="Server shutdown"
     send_broadcast_message "The server shutting down..."
-    [ -n "${REASON}" ] && send_broadcast_message "Reason: ${REASON}"
 
+    if [ -n "${REASON}" ]; then
+        send_broadcast_message "Reason: ${REASON}"
+        KICK_REASON="${KICK_REASON} due to: ${REASON}"
+    fi
+
+    run_server_command kickall "${KICK_REASON}"
     papermc stop
 fi
