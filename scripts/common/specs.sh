23ed0e52ad3d86b24cc5f75651d50470e7c2f423
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2024-07-04 10:10:14 +0300

    Updated to 1.20.6

--- scripts/common/specs.sh
+++ scripts/common/specs.sh
@@ -14,7 +14,7 @@ function get_server_property() {
     echo "${VALUE}"
 }
 
-export MINECRAFT_VERSION="1.20.4"
+export MINECRAFT_VERSION="1.20.6"
 export ONLINE_MODE=$(get_server_property "online-mode" "false")
 export WORLD_NAME=$(get_server_property "level-name" "world")
 
