a8467b8c86123240d60d3defb47445bd9f62b706
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2024-09-14 17:32:17 +0300

    Support romanian locale in the stop script

--- stop.sh
+++ stop.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
 SERVER_ROOT_DIR=$(pwd)
 source "${SERVER_ROOT_DIR}/scripts/common/paths.sh"
+source "${SERVER_ROOT_DIR}/scripts/common/specsq.sh"
 source "${SERVER_SCRIPTS_COMMON_DIR}/status.sh"
 source "${SERVER_SCRIPTS_COMMON_DIR}/utils.sh"
 
 REASON="${*}"
 SECONDS_BEFORE_KICK=30
 
+BROADCAST_HEADER_MESSAGE='Attention everyone'
+BROADCAST_SHUTDOWN_MESSAGE='The server will shut down in a moment'
+BROADCAST_KICK_MESSAGE="All online players will be kicked in ${SECONDS_BEFORE_KICK} seconds"
+KICK_REASON='Server shutdown'
+
+if [ "${LOCALE}" = 'ro' ]; then
+    BROADCAST_HEADER_MESSAGE='Atenție toată lumea'
+    BROADCAST_SHUTDOWN_MESSAGE='Server-ul se va opri într-un moment'
+    BROADCAST_KICK_MESSAGE="Toți jucătorii online vor fi deconectați în ${SECONDS_BEFORE_KICK} secunde"
+    KICK_REASON='Server-ul se oprește'
+fi
+
 if ${IS_SERVER_RUNNING}; then
-    KICK_REASON="Server shutdown"
+    run_server_command "essentials:broadcast ${BROADCAST_HEADER_MESSAGE}!"
 
-    run_server_command 'essentials:broadcast Attention everyone!'
-
-    send_broadcast_message "The server will be shutting down in a moment..."
+    send_broadcast_message "${BROADCAST_SHUTDOWN_MESSAGE}..."
     if [ -n "${REASON}" ]; then
-        send_broadcast_message "Reason: ${REASON}"
-        KICK_REASON="${KICK_REASON} due to: ${REASON}"
+        if [ "${LOCALE}" = 'ro' ]; then
+            send_broadcast_message "Motiv: ${REASON}"
+            KICK_REASON="${KICK_REASON} pentru: ${REASON}"
+        else
+            send_broadcast_message "Reason: ${REASON}"
+            KICK_REASON="${KICK_REASON} due to: ${REASON}"
+        fi
     fi
 
-    run_server_command "essentials:broadcast All online players will be kicked in ${SECONDS_BEFORE_KICK} seconds!"
+    run_server_command "essentials:broadcast ${BROADCAST_KICK_MESSAGE}!"
 
     sleep "${SECONDS_BEFORE_KICK}"
 
